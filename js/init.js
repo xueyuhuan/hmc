ae141b2773fb64eed6ec8a0b589076eb1c30f484
xueyuhuan <501946152@qq.com>
2019-08-30 17:54:35 +0800

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,20 @@
+$(function() {
+    $(".enter").load("enter.html");
+    // $("#partners").load("partners.html");
+    $("footer").load("footer.html");
+    $("nav").load("header.html", function () {
+        let links = $("header li a"),
+            index = 0, //默认第一个菜单项
+            //取当前URL最后一个/后面的文件名，pop方法是删除最后一个元素并返回最后一个元素
+            url = location.href.split("?")[0].split("/").pop();
+        if (url) {//如果有取到，则进行匹配，否则默认首页（即index所指向的那个）
+            for (var i = 0; i < links.length; i++) {//遍历menu中的链接地址
+                if (links[i].href.indexOf(url) != -1) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        $("header li a").eq(index).addClass("active");//给对应的<li>增加选中样式
+    });
+});
